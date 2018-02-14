27cc20eb031905099117c4138e43229be5326569
Dylan Law <law.dylan.t@gmail.com>
2018-02-14 14:31:45 -0500

    Implement binary control interface and button

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour, BinaryControl {
+
+	// Exposed variables
+	[ SerializeField ]
+	private float _depressDepth;
+
+
+	// Private variables
+	private Transform _transform;
+
+	private bool _state;
+
+
+	// Messages
+	private void Awake() {
+		_transform = GetComponent< Transform >();
+		_state = false;
+	}
+
+	private void OnMouseDown() {
+
+		// TODO: Change to actual animation
+		_transform.position -= _transform.up * _depressDepth;
+		_state = true;
+	}
+
+	private void OnMouseUp() {
+
+		// TODO: Change to actual animation
+		_transform.position += _transform.up * _depressDepth;
+		_state = false;
+	}
+
+
+	// Public interface
+	public bool GetState() {
+		return _state;
+	}
+}
