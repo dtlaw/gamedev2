25a2153c4289c378d059c38a06636e29f84768a5
Dylan Law <law.dylan.t@gmail.com>
2018-02-20 23:40:38 -0500

    Change movement to have independent torque values

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,84 +5,90 @@ using UnityEngine;
 [ RequireComponent( typeof( Rigidbody )) ]
 public class Movement : MonoBehaviour {
 
-    // Exposed variables
-    [ SerializeField ] private float _translationForce;
-    [ SerializeField ] private float _rotationTorque;
+	// Exposed variables
+	[ Header( "Movement forces" ) ]
+	[ SerializeField ]
+	private float _translationForce;
+	[ SerializeField ]
+	private float _pitchTorque;
+	[ SerializeField ]
+	private float _rollTorque;
+	[ SerializeField ]
+	private float _yawTorque;
 
-    [ Header( "Movement buttons" ) ]
-    [ SerializeField ]
-    private Control _forward;
-    [ SerializeField ]
-    private Control _backward;
-    [ SerializeField ]
-    private Control _strafeLeft;
-    [ SerializeField ]
-    private Control _strafeRight;
-    [ SerializeField ]
-    private Control _moveUp;
-    [ SerializeField ]
-    private Control _moveDown;
+	[ Header( "Movement buttons" ) ]
+	[ SerializeField ]
+	private Control _forward;
+	[ SerializeField ]
+	private Control _backward;
+	[ SerializeField ]
+	private Control _strafeLeft;
+	[ SerializeField ]
+	private Control _strafeRight;
+	[ SerializeField ]
+	private Control _moveUp;
+	[ SerializeField ]
+	private Control _moveDown;
 
-    [ Space ]
-    [ SerializeField ]
-    private Control _pitchUp;
-    [ SerializeField ]
-    private Control _pitchDown;
-    [ SerializeField ]
-    private Control _rollLeft;
-    [ SerializeField ]
-    private Control _rollRight;
-    [ SerializeField ]
-    private Control _yawLeft;
-    [ SerializeField ]
-    private Control _yawRight;
-
-
-    // Private variables
-    Rigidbody _rigidbody;
+	[ Space ]
+	[ SerializeField ]
+	private Control _pitchUp;
+	[ SerializeField ]
+	private Control _pitchDown;
+	[ SerializeField ]
+	private Control _rollLeft;
+	[ SerializeField ]
+	private Control _rollRight;
+	[ SerializeField ]
+	private Control _yawLeft;
+	[ SerializeField ]
+	private Control _yawRight;
 
 
-    // Messages
-    private void Awake() {
-        _rigidbody = GetComponent< Rigidbody >();
+	// Private variables
+	Rigidbody _rigidbody;
+
+
+	// Messages
+	private void Awake() {
+		_rigidbody = GetComponent< Rigidbody >();
 	}
 	
 	private void Update() {
-        if ( _forward.IsOn()) {
-            _rigidbody.AddRelativeForce(Vector3.forward * _translationForce);
-        } else if ( _backward.IsOn()) {
-            _rigidbody.AddRelativeForce(Vector3.back * _translationForce);
-        }
+		if ( _forward.IsOn()) {
+			_rigidbody.AddRelativeForce( Vector3.forward * _translationForce );
+		} else if ( _backward.IsOn()) {
+			_rigidbody.AddRelativeForce( Vector3.back * _translationForce );
+		}
 
-        if ( _strafeLeft.IsOn()) {
-            _rigidbody.AddRelativeForce(Vector3.left * _translationForce);
-        } else if ( _strafeRight.IsOn()) {
-            _rigidbody.AddRelativeForce(Vector3.right * _translationForce);
-        }
+		if ( _strafeLeft.IsOn()) {
+			_rigidbody.AddRelativeForce( Vector3.left * _translationForce );
+		} else if ( _strafeRight.IsOn()) {
+			_rigidbody.AddRelativeForce( Vector3.right * _translationForce );
+		}
 
-        if ( _moveUp.IsOn()) {
-            _rigidbody.AddRelativeForce( Vector3.up * _translationForce );
-        } else if ( _moveDown.IsOn()) {
-            _rigidbody.AddRelativeForce( Vector3.down * _translationForce );
-        }
+		if ( _moveUp.IsOn()) {
+			_rigidbody.AddRelativeForce( Vector3.up * _translationForce );
+		} else if ( _moveDown.IsOn()) {
+			_rigidbody.AddRelativeForce( Vector3.down * _translationForce );
+		}
 
+		if ( _pitchUp.IsOn()) {
+			_rigidbody.AddRelativeTorque( -_pitchTorque, 0, 0 );
+		} else if ( _pitchDown.IsOn()) {
+			_rigidbody.AddRelativeTorque( _pitchTorque, 0, 0 );
+		}
 
-        if ( _pitchUp.IsOn()) {
-            _rigidbody.AddRelativeTorque( -_rotationTorque, 0, 0 );
-        } else if ( _pitchDown.IsOn()) {
-            _rigidbody.AddRelativeTorque( _rotationTorque, 0, 0 );
-        }
+		if ( _rollLeft.IsOn()) {
+			_rigidbody.AddRelativeTorque( 0, 0, _rollTorque );
+		} else if ( _rollRight.IsOn()) {
+			_rigidbody.AddRelativeTorque( 0, 0, -_rollTorque );
+		}
 
-        if ( _rollLeft.IsOn()) {
-            _rigidbody.AddRelativeTorque( 0, 0, _rotationTorque );
-        } else if ( _rollRight.IsOn()) {
-            _rigidbody.AddRelativeTorque( 0, 0, -_rotationTorque );
-        }
-
-        if ( _yawLeft.IsOn()) {
-            _rigidbody.AddRelativeTorque( 0, _rotationTorque, 0);
-        } else if ( _yawRight.IsOn()) {
-            _rigidbody.AddRelativeTorque( 0, -_rotationTorque, 0 );
-        }
-    }
+		if ( _yawLeft.IsOn()) {
+			_rigidbody.AddRelativeTorque( 0, _yawTorque, 0);
+		} else if ( _yawRight.IsOn()) {
+			_rigidbody.AddRelativeTorque( 0, -_yawTorque, 0 );
+		}
+	}
 }
