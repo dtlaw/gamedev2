a7cedab83f82dfc982cf890a0cda0cb5c72f5918
AnIllegalPanda <danhendricks36@gmail.com>
2018-02-21 01:23:53 -0500

    Added Doors to Test Scene

---
+++ Assets/Scripts/DoorControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _door;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _door.gameObject.SetActive(false);
+    }
+}
