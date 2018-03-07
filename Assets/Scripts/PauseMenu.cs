880ec53f72d3586b521b714ed437643aadf7f1b0
AnIllegalPanda <danhendricks36@gmail.com>
2018-03-07 13:41:47 -0500

    Added Menu Scene and Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    private static bool _gameIsPaused = false;
+
+    [SerializeField]
+    private GameObject _pauseMenuUI;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    public void Resume()
+    {
+        _pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        _gameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        _pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        _gameIsPaused = true;
+    }
+    public void Menu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
