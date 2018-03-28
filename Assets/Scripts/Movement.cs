1bedaba6001d0998ffa017e60c4eb49537cfe266
anniepa <anniepa246@gmail.com>
2018-03-27 23:28:50 -0400

    adjusted new models
    new arm models

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -57,8 +57,6 @@ public class Movement : MonoBehaviour
     [SerializeField]
     private GameObject _forearm;
     [SerializeField]
-    private GameObject _wrist;
-    [SerializeField]
     private GameObject _hand;
 
     [Header("Thruster Particles")]
@@ -96,7 +94,6 @@ public class Movement : MonoBehaviour
         _shoulderbody = _shoulder.GetComponent<Rigidbody>();
         _forearmbody = _forearm.GetComponent<Rigidbody>();
         _handbody = _hand.GetComponent<Rigidbody>();
-        _wristbody = _wrist.GetComponent<Rigidbody>();
     }
 
     private void Update()
@@ -110,20 +107,20 @@ public class Movement : MonoBehaviour
         _thruster7.Stop();
         _thruster8.Stop();
         if (_shoulder.GetComponent<shoulderBehavior>().use == false &&
-            _forearm.GetComponent<forearmBehavior>().use == false &&
-            _wrist.GetComponent<wristBehavior>().use == false)
-        {
+            _forearm.GetComponent<forearmBehavior>().use == false){
             _shoulderbody.constraints = RigidbodyConstraints.FreezePosition;
             _forearmbody.constraints = RigidbodyConstraints.FreezePosition;
             _handbody.constraints = RigidbodyConstraints.FreezePosition;
-            _wristbody.constraints = RigidbodyConstraints.FreezePosition;
+
+			// _shoulderbody.constraints = RigidbodyConstraints.FreezeRotation;
+			// _forearmbody.constraints = RigidbodyConstraints.FreezeRotation;
+			// _handbody.constraints = RigidbodyConstraints.FreezeRotation;
+			
         }
-        else
-        {
+        else{
             _shoulderbody.constraints = RigidbodyConstraints.None;
             _forearmbody.constraints = RigidbodyConstraints.None;
             _handbody.constraints = RigidbodyConstraints.None;
-            _wristbody.constraints = RigidbodyConstraints.None;
         }
         if (_forward.IsOn())
         {
