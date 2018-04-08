b9b173c70e122422d9588e8f97a63a5ced64b9e4
limy97 <32105571+limy97@users.noreply.github.com>
2018-04-08 19:32:48 -0400

    transparent curved UI

---
+++ Assets/Scripts/FlipNormal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FlipNormal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var mesh = (transform.GetComponent("MeshFilter") as MeshFilter).sharedMesh;
+		mesh.uv = mesh.uv.Select(o => new Vector2(1 - o.x, o.y)).ToArray();
+		mesh.triangles = mesh.triangles.Reverse().ToArray();
+		mesh.normals = mesh.normals.Select(o => -o).ToArray();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
