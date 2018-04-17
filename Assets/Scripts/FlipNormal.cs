8887092c4cc8f1628d817411a0a3bc64e3acd168
limy97 <32105571+limy97@users.noreply.github.com>
2018-04-17 19:12:39 -0400

    fix hud not showing up problem

--- Assets/Scripts/FlipNormal.cs
+++ Assets/Scripts/FlipNormal.cs
@@ -7,7 +7,7 @@ public class FlipNormal : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		var mesh = (transform.GetComponent("MeshFilter") as MeshFilter).sharedMesh;
+		var mesh = (transform.GetComponent("MeshFilter") as MeshFilter).mesh;
 		mesh.uv = mesh.uv.Select(o => new Vector2(1 - o.x, o.y)).ToArray();
 		mesh.triangles = mesh.triangles.Reverse().ToArray();
 		mesh.normals = mesh.normals.Select(o => -o).ToArray();
