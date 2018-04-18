cce433fd416216bfc53d2e9a75a7b46653f73196
AnIllegalPanda <danhendricks36@gmail.com>
2018-04-18 00:07:53 -0400

    FullBuild Almsost Ready

--- Assets/Scripts/SnapMovement2.cs
+++ Assets/Scripts/SnapMovement2.cs
@@ -2,23 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnapMovement : MonoBehaviour {
-	private Vector3 _doorPos;
-	[ SerializeField ]
-	private GameObject goal;
+public class SnapMovement2 : MonoBehaviour
+{
+    private Vector3 _doorPos;
+    [SerializeField]
+    private GameObject goal;
 
-	[ SerializeField ]
-	private GameObject door;
+    [SerializeField]
+    private GameObject door;
 
+    bool _snapped;
     private Hand _hand;
-	// Use this for initialization
-	void Start () {
-		_doorPos = door.transform.position;
+
+    // Use this for initialization
+    void Start()
+    {
+        _snapped = false;
+        _doorPos = door.transform.position;
         _hand = GameManager.Instance.GetComponent<CockpitReferences>().Hand;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
 
         if (Vector3.Distance(_hand.transform.position, goal.transform.position) <= 1.3)
         {
@@ -26,12 +32,18 @@ public class SnapMovement : MonoBehaviour {
             {
                 transform.rotation = Quaternion.Euler(new Vector3(goal.transform.rotation.eulerAngles.x, goal.transform.rotation.eulerAngles.y, goal.transform.rotation.eulerAngles.z));
                 transform.position = Vector3.Lerp(transform.position, goal.transform.position, Time.time);
-                door.transform.position = Vector3.Lerp(door.transform.position, _doorPos + (door.transform.right * 3.5f), 0.10f);
+                _snapped = true;
             }
             else
             {
                 door.transform.position = Vector3.Lerp(door.transform.position, _doorPos, 0.10f);
+                _snapped = false;
             }
         }
+    }
+
+    public bool isSnapped()
+    {
+        return _snapped;
     }
 }
