c4e3517d55f3f3d68925da6973cabeff71353f1b
AnIllegalPanda <danhendricks36@gmail.com>
2018-04-18 12:20:36 -0400

    Almost full working

--- Assets/Scripts/SnapMovement.cs
+++ Assets/Scripts/SnapMovement.cs
@@ -9,17 +9,20 @@ public class SnapMovement : MonoBehaviour {
 
 	[ SerializeField ]
 	private GameObject door;
+
+    private Hand _hand;
 	// Use this for initialization
 	void Start () {
 		_doorPos = door.transform.position;
+        _hand = GameManager.Instance.GetComponent<CockpitReferences>().Hand;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Vector3.Distance(GameObject.Find("Hand").transform.position, goal.transform.position) <= 1.3)
+        if (Vector3.Distance(_hand.transform.position, goal.transform.position) <= 1.3)
         {
-            if (!GameObject.Find("Hand").GetComponent<handBehavior>().Grab())
+            if (!_hand.Grabbing)
             {
                 transform.rotation = Quaternion.Euler(new Vector3(goal.transform.rotation.eulerAngles.x, goal.transform.rotation.eulerAngles.y, goal.transform.rotation.eulerAngles.z));
                 transform.position = Vector3.Lerp(transform.position, goal.transform.position, Time.time);
