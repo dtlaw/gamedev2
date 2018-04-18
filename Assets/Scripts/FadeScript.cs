5c58a8036468e120235313fa46fc0dc50871de61
AnIllegalPanda <danhendricks36@gmail.com>
2018-04-18 01:30:27 -0400

    Attempt Merge

---
+++ Assets/Scripts/FadeScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject Image;
+
+    private Animation Fade;
+
+    private void Awake()
+    {
+        Fade = Image.GetComponent<Animation>();
+    }
+
+    void OnTriggerEnter(Collider Other)
+    {
+        Fade.Play("FadeOut");
+    }
+
+}
