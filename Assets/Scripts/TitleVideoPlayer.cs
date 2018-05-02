02fa5e8071c610e89aeec13c1d2d7d5b62255f2b
AnIllegalPanda <danhendricks36@gmail.com>
2018-05-01 21:03:40 -0400

    Added Rotation in Menu Scene

---
+++ Assets/Scripts/TitleVideoPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleVideoPlayer : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject camera = GameObject.Find("Main Camera");
+        UnityEngine.Video.VideoPlayer videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.playOnAwake = true;
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+        videoPlayer.targetCameraAlpha = 0.5F;
+        videoPlayer.isLooping = true;
+    }
+}
