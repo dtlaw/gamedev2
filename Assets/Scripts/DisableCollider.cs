e32d74e032df2077b3753ee523c24bb7e3f2f15e
limy97 <32105571+limy97@users.noreply.github.com>
2018-05-04 01:58:57 -0400

    added death particles, hand cannot get into bars, colliders disabled for beam

--- Assets/Scripts/DisableCollider.cs
+++ Assets/Scripts/DisableCollider.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DisableCollider : MonoBehaviour {
-
+	// public variables
+	public ParticleSystem deathParticles;
 	// Private variables
 	private Collider _collider;
 
 	private Hand _hand;
 	private Movement _movement;
+	private ParticleSystem _self;
 
 
 	// Messages
 	private void Awake() {
 		_collider = GetComponent< Collider >();
+		_self = GetComponent< ParticleSystem >();
 		
 	}
 
 	private void Start() {
+		_collider.enabled = false;
 		_hand = GameManager.Instance.GetComponent< CockpitReferences >().Hand;
 		_movement = GameManager.Instance.GetComponent< CockpitReferences >().Movement;
 	}
@@ -25,6 +29,9 @@ public class DisableCollider : MonoBehaviour {
 	private void Update() {
 		if ( Vector3.Distance( transform.position, _hand.transform.position) < 0.8f ) {
 			_movement.StepBack();
+			// particle systems for collision
+			deathParticles.startColor = _self.startColor;
+			Instantiate(deathParticles, _hand.transform.position, Quaternion.identity);
 		}
 	}
 
